7d94143747adcf0d3d1cf8a2d35c6c55cf5cf128
Nikita <58188102+NickDesire@users.noreply.github.com>
2021-03-12 00:04:19 +0300

    docs: update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if( n < 0){
+        a = Math.abs(n);
+        return parseInt(a.toString().split('').reverse().join('')) * Math.sign(a)
+    }
+    return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n)
 }
